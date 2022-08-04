4d4155b3ab6a79a304707e8791d22d3f2fb4428b
Gisele <gisele.viedenhelfen@gmail.com>
2022-08-04 11:50:22 -0300

    estabelece condições de login

--- index.js
+++ index.js
@@ -26,10 +26,21 @@ app.get('/talker/:id', (req, res) => {
 
   return res.status(HTTP_OK_STATUS).json(idTalker);
 });
+
 app.post('/login', (req, res) => {
-  // const { email, password } = req.body;
-  // const re = /\S+@\S+\.\S+/;
+  const regex = /\S+@\S+\.\S+/;
   const token = crypto.randomBytes(8).toString('hex');
+  const { email, password } = req.body;
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  if (email === undefined) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' }); 
+}
+  if (!regex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+}
   return res.status(200).json({ token });
 });
 app.listen(PORT, () => {
