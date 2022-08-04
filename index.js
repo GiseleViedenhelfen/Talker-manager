148cec7cce57270bdc2dc0e57f2ab0657e242c3e
Gisele <gisele.viedenhelfen@gmail.com>
2022-08-04 10:33:03 -0300

    começando o proj vqv

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
-
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const talkerJSON = './talker.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
-
+app.get('/talker', (req, res) => {
+  const docJSON = fs.readFileSync(talkerJSON, 'utf-8');
+  const docJsonParse = JSON.parse(docJSON);
+ return res.status(HTTP_OK_STATUS).json(docJsonParse);
+});
 app.listen(PORT, () => {
   console.log('Online');
 });
